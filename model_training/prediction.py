2a220afedae8de3e892415679c09f1163f372baa
guo-ganggang <55788288+guo-ganggang@users.noreply.github.com>
2020-08-30 22:39:41 +0800

    Add files via upload

---
+++ model_training/prediction.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 8/11/2017 上午 11:17
+# @Author  : GUO Ganggang
+# @email   : ganggangguo@csu.edu.cn
+# @Site    : 
+# @File    : prediction.py
+# @Software: PyCharm
+
+import eval
+
+eval.prediction('1')
